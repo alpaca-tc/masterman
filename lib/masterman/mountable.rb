a5f85a07d906a872f73b9846fd54bed9b9b3bd94
alpaca-tc <alpaca-tc@alpaca.tc>
2015-12-30 20:23:23 +0900

    Raise error when missing mount option

--- lib/masterman/mountable.rb
+++ lib/masterman/mountable.rb
@@ -1,7 +1,6 @@
 module Masterman
   module Mountable
     attr_writer :primary_key
-    attr_reader :loader
 
     def mount(options = {})
       @loader = Loader.build(options)
@@ -13,6 +12,10 @@ module Masterman
 
     def all
       to_instances(loader.all)
+    end
+
+    def loader
+      @loader || MastermanError.new('not mounted or bad option given')
     end
 
     private
